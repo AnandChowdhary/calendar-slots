553f6c5bac5849ba94d6d921d886fbacce757677
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-03 05:04:38 +0530

    :construction: Work on list of events

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
 import { google } from "googleapis";
+import {
+  OAuth2Client,
+  JWT,
+  Compute,
+  UserRefreshClient,
+} from "google-auth-library";
 import { config } from "dotenv";
 config();
 
@@ -9,17 +15,53 @@ const oauth2Client = new google.auth.OAuth2(
 );
 const calendar = google.calendar("v3");
 
-export const getSlots = async ({
-  user,
-}: {
+export interface IGetEventsParams {
   user?: {
     accessToken: string;
     refreshToken: string;
   };
-}) => {
+  from?: Date;
+  to?: Date;
+  calendarId?: string;
+  maxAttendees?: number;
+  auth?: string | OAuth2Client | JWT | Compute | UserRefreshClient;
+  timeMax?: string;
+  timeMin?: string;
+  timeZone?: string;
+}
+
+/*
+ * Find a user's events
+ */
+export const getEventsFromSingleCalendar = async ({
+  user,
+  from,
+  to,
+  calendarId,
+  maxAttendees,
+  auth,
+  timeMax,
+  timeMin,
+  timeZone,
+}: IGetEventsParams) => {
   oauth2Client.setCredentials({
     access_token: user?.accessToken || process.env.GOOGLE_DOCS_ACCESS,
     refresh_token: user?.refreshToken || process.env.GOOGLE_DOCS_REFRESH,
   });
-  return true;
+  return calendar.events.list({
+    timeMin: from?.toISOString(),
+    timeMax: to?.toISOString(),
+    auth: auth || oauth2Client,
+    calendarId: calendarId || "primary",
+    timeZone,
+    maxResults: 3,
+  });
 };
+
+export const getSlots = async () => true;
+
+const test = async () => {
+  const result = await getEventsFromSingleCalendar({});
+  console.log("RESULT", result);
+};
+test();
